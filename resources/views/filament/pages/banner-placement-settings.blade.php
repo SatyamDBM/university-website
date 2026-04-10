6735f778cb99a5e7b93a887bf22de2f376981082
dbm.laraveldeveloper <dbm.laraveldeveloper@gmail.com>
2026-04-10 17:11:20 +0530

    banners

--- resources/views/filament/pages/banner-placement-settings.blade.php
+++ resources/views/filament/pages/banner-placement-settings.blade.php
@@ -1,3 +1,21 @@
 <x-filament-panels::page>
-    {{ $this->table }}
+    <div class="space-y-4">
+        <div class="rounded">
+            <div class="flex items-center justify-between px-6 py-1">
+                <div>
+                    <h2 class="font-semibold text-[var(--color-text-dark)]">
+                        University Banners
+                    </h2>
+
+                    <p class="text-xs text-[var(--color-text-subtle)]">
+                        View and manage all banners purchased by universities.
+                    </p>
+                </div>
+            </div>
+
+            <div class="p-2">
+                {{ $this->table }}
+            </div>
+        </div>
+    </div>
 </x-filament-panels::page>
